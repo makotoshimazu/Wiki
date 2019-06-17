01d13f904f79200eb7f1dfa7a42d3321ee42e34f
morux2 <k-morimoto@keio.jp>
2019-06-17 20:40:11 +0900

    再起dfs ver2

--- src/wiki/wiki.java
+++ src/wiki/wiki.java
@@ -8,26 +8,30 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
 
+//TODO コマンドラインで始点と終点を指定する
+//プログラムの実行方法 $ java -Xss16m wiki/wiki (これでスタックサイズを指定)
+
 public class wiki {
 	final static int LEN = 1483277;
+	final static int MAX_DEPTH = 30;
 	static page pages[] = new page[LEN];
 
 	public static void main(String[] args) {
 		readPage();
 		readLink();
-		int startIndex = getIndexOf("アンパサンド");
-		int targetIndex = getIndexOf("プログラミング言語");
+		int startIndex = getIndexOf("六本木ヒルズ森タワー");
+		int targetIndex = getIndexOf("スクランブル交差点");
 		isNotFound(startIndex);
 		isNotFound(targetIndex);
-		int answerIndex = dfs(startIndex, targetIndex);
-		isNotFound(answerIndex);
+		dfs(startIndex, targetIndex, -1);
+		System.out.printf("not found\n");
 	}
 
 	static void readPage() {
 		int index = 0;
 		try {
 			//pagesファイルを指定
-			File file = new File("src/pages.txt");
+			File file = new File("./pages.txt");
 			//BufferReaderは1行ずつ読み込む
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
 			//nullまで読み込む
@@ -55,31 +59,31 @@ public class wiki {
 			e.printStackTrace();
 		}
 		/*
-				//debug用にoutputStreamに書き込み
-				try {
-					File file = new File("src/pageArray.txt");
-					BufferedWriter bufferWriter = new BufferedWriter(new FileWriter(file));
-					for (page p : pages) {
-						bufferWriter.write(p.title);
-						//改行コードをOS似合わせて自動で判断して出力
-						bufferWriter.newLine();
-					}
-					bufferWriter.close();
-					//ファイルが見つからなければエラー
-				} catch (FileNotFoundException e) {
-					System.out.println(e);
-					//writeでエラーが出た時
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				*/
+		//debug用にoutputStreamに書き込み
+		try {
+			File file = new File("./pageArray.txt");
+			BufferedWriter bufferWriter = new BufferedWriter(new FileWriter(file));
+			for (page p : pages) {
+				bufferWriter.write(p.title);
+				//改行コードをOS似合わせて自動で判断して出力
+				bufferWriter.newLine();
+			}
+			bufferWriter.close();
+			//ファイルが見つからなければエラー
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+			//writeでエラーが出た時
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		*/
 
 	}
 
 	static void readLink() {
 		try {
 			//pagesファイルを指定
-			File file = new File("src/links.txt");
+			File file = new File("./links.txt");
 			//BufferReaderは1行ずつ読み込む
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
 			//nullまで読み込む
@@ -103,7 +107,7 @@ public class wiki {
 		/*
 		//debug用にoutputStreamに書き込み
 		try {
-			File file = new File("src/linkList.txt");
+			File file = new File("./linkList.txt");
 			BufferedWriter bufferWriter = new BufferedWriter(new FileWriter(file));
 			for (int p : pages[0].reference) {
 				bufferWriter.write(pages[p].title);
@@ -136,27 +140,24 @@ public class wiki {
 		}
 	}
 
-	//pから探し始める
-	static int dfs(int start, int target) {
+	//TODO 再起の深さを計算する、深さがリミットを超えたらそれ以上再起をしない
+	static void dfs(int start, int target, int depth) {
+		depth++;
 		//visitで訪問済みかどうか管理する
-		stack stack = new stack();
-		stack.push(start);
-		while (stack.count() > -1) {
-			int current = stack.pop();
-			pages[current].visited = true;
-			System.out.printf("%s --> \n", pages[current].title);
-			if (pages[current].index == target) {
-				System.out.println("found!");
-				return pages[current].index;
-			}
-			Iterator<Integer> itr = pages[current].reference.iterator();
-			while (itr.hasNext()) {
-				int next = itr.next();
-				if (pages[next].visited != true) {
-					stack.push(next);
+		pages[start].visited = true;
+		Iterator<Integer> itr = pages[start].reference.iterator();
+		while (itr.hasNext()) {
+			int num = itr.next();
+			if (num == target) {
+				System.out.printf("found! %s --> %s \n", pages[start].title, pages[num].title);
+				System.exit(0);
+				break;
+			} else {
+				if (pages[num].visited == false && depth < MAX_DEPTH) {
+					System.out.printf("%s --> %s \n", pages[start].title, pages[num].title);
+					dfs(num, target, depth);
 				}
 			}
 		}
-		return -1;
 	}
 }
